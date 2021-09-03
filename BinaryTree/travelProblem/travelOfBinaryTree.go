9918f153fabdbb7e7789c149d97c3bc9220eb8f4
qiujun <qiujun@travelsky.com>
2021-09-03 23:39:55 +0800

    level order in complex situations

--- BinaryTree/travelProblem/travelOfBinaryTree.go
+++ BinaryTree/travelProblem/travelOfBinaryTree.go
@@ -199,3 +199,83 @@ func LevelOrderComplex(root *TreeNode) [][]int {
 	}
 	return res
 }
+
+/*
+2.3 给定一个二叉树，返回其节点值自底向上的层序遍历。（即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+以上面的示例二叉树为例，最后应返回[[7,2,5,1],[11,13,4],[4,8],[5]]
+ */
+
+// LevelOrderBottom, 与2.2类似，将得到的结果逆序即可满足要求
+func LevelOrderBottom(root *TreeNode)[][]int{
+	var res [][]int
+	if root == nil{
+		return res
+	}
+	queue := []*TreeNode{root}
+	level := 0
+	for len(queue) != 0{
+		var temp []*TreeNode
+		res = append(res, []int{})
+		for _, node := range queue{
+			res[level] = append(res[level], node.Val)
+			if node.Left != nil{
+				temp = append(temp, node.Left)
+			}
+			if node.Right != nil{
+				temp = append(temp, node.Right)
+			}
+		}
+		level++
+		queue = temp
+	}
+
+	return ReverseComplexArray(res)
+}
+
+func ReverseComplexArray(src [][]int)[][]int{
+	length := len(src)
+	for i:=0;i<length/2;i++{
+		temp := src[length-1-i]
+		src[length-1-i] = src[i]
+		src[i] = temp
+	}
+	return src
+}
+
+/*
+二叉树的锯齿形层序遍历
+2.4 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，
+层与层之间交替进行）。以上面的示例二叉树为例，最后应返回[[5],[8,4],[11,13,4],[1,5,2,7]]
+ */
+
+// zigzagLevelOrder, 与与2.2类似，所不同的是要加入层数(level)的判断，根据层数来确定是否要对该层的节点值进行逆序
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	var res [][]int
+	if root == nil {
+		return res
+	}
+	queue := []*TreeNode{root}
+	symbol := 0
+	for len(queue) != 0 {
+		levelSize := len(queue)
+		var curLevel []int
+		for i:=0;i<levelSize;i++{
+			node := queue[0]
+			queue = queue[1:]
+			curLevel = append(curLevel, node.Val)
+			if node.Left != nil{
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil{
+				queue = append(queue, node.Right)
+			}
+		}
+		symbol++
+		if symbol % 2 == 0{
+			res = append(res, ReverseArray(curLevel))
+		} else{
+			res = append(res, curLevel)
+		}
+	}
+	return res
+}
