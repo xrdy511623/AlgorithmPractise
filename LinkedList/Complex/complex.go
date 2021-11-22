3e4e7851d79a71c97ae13a4dd0a74f9ca3b9f566
qiujun <qiujun@rcrai.com>
2021-11-22 17:56:22 +0800

    add new examples

--- LinkedList/Complex/complex.go
+++ LinkedList/Complex/complex.go
@@ -197,3 +197,44 @@ func MergeTwoLists(a, b *Entity.ListNode) *Entity.ListNode {
 	}
 	return dummy.Next
 }
+
+/*
+1.3 两数相加
+给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字0之外，这两个数都不会以0开头。
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+ */
+
+// AddTwoNumbers 时间复杂度O(max(m,n)), m和n分别为两个链表的长度，空间复杂度O(1)
+func AddTwoNumbers(l1, l2 *Entity.ListNode)(head *Entity.ListNode){
+	var ndx *Entity.ListNode
+	// 进位值初始值为0
+	carry := 0
+	for l1 != nil || l2 != nil{
+		n1, n2 := 0, 0
+		if l1 != nil{
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil{
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := n1 + n2 + carry
+		sum, carry = sum % 10, sum/10
+		if head == nil{
+			head = &Entity.ListNode{Val:sum}
+			ndx = head
+		} else{
+			ndx.Next = &Entity.ListNode{Val:sum}
+			ndx = ndx.Next
+		}
+	}
+	if carry > 0{
+		ndx.Next = &Entity.ListNode{Val:carry}
+	}
+	return
+}
