036f080f0da9207b91b85a3d93bee47fc47d562c
qiujun <qiujun@rcrai.com>
2021-11-30 10:09:28 +0800

    add new examples

--- BinaryTree/featureProblem/feature.go
+++ BinaryTree/featureProblem/feature.go
@@ -503,3 +503,48 @@ func IncreasingSimpleBST(root *Entity.TreeNode)*Entity.TreeNode{
 	helper(root)
 	return dummyNode.Right
 }
+
+
+/*
+1.12 二叉搜索树迭代器
+实现一个二叉搜索树迭代器类BSTIterator，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+BSTIterator(TreeNode root) 初始化BSTIterator类的一个对象。BST的根节点root会作为构造函数的一部分给出。指针应初始化为一个不存在于
+BST中的数字，且该数字小于BST中的任何元素。
+boolean hasNext() 如果向指针右侧遍历存在数字，则返回true ；否则返回false 。
+int next()将指针向右移动，然后返回指针处的数字。
+注意，指针初始化为一个不存在于BST中的数字，所以对next()的首次调用将返回 BST 中的最小元素。
+你可以假设next()调用总是有效的，也就是说，当调用 next()时，BST的中序遍历中至少存在一个下一个数字。
+ */
+
+type BSTIterator struct {
+	Nums []int
+	Root *Entity.TreeNode
+}
+
+func Constructor(root *Entity.TreeNode)*BSTIterator{
+	nums := make([]int, 0)
+	Inorder(root, &nums)
+	return &BSTIterator{
+		Nums: nums,
+		Root: root,
+	}
+}
+
+func(this *BSTIterator)Next()int{
+	val := this.Nums[0]
+	this.Nums = this.Nums[1:]
+	return val
+}
+
+func(this *BSTIterator) HasNext()bool{
+	return len(this.Nums) > 0
+}
+
+func Inorder(node *Entity.TreeNode, nums *[]int){
+	if node == nil{
+		return
+	}
+	Inorder(node.Left, nums)
+	*nums = append(*nums, node.Val)
+	Inorder(node.Right, nums)
+}
