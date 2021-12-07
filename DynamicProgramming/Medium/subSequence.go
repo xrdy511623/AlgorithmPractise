d5f65ff2a0355f15a0733a65f8ef897ca74b88fb
qiujun <qiujun@travelsky.com>
2021-12-07 21:52:45 +0800

    modify a little

--- DynamicProgramming/Medium/subSequence.go
+++ DynamicProgramming/Medium/subSequence.go
@@ -3,7 +3,6 @@ package Medium
 import "AlgorithmPractise/Utils"
 
 /*
-subSequence.go
 1.1 最长递增子序列
 给你一个整数数组nums ，找到其中最长严格递增子序列的长度。
 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7]
