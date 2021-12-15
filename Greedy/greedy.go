6c69bee3f5aa21719d839dfbd228151b70c7558e
qiujun <qiujun@travelsky.com>
2021-12-15 18:11:57 +0800

    add new examples

--- Greedy/greedy.go
+++ Greedy/greedy.go
@@ -867,3 +867,48 @@ func PartitionLabels(s string) []int {
 	}
 	return res
 }
+
+/*
+1.15 合并区间
+给出一个区间的集合，请合并所有重叠的区间。
+
+示例1:
+输入: intervals = [[1,3],[2,6],[8,10],[15,18]] 输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+
+示例2:
+输入: intervals = [[1,4],[4,5]] 输出: [[1,5]]
+解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+提示：
+intervals[i][0] <= intervals[i][1]
+*/
+
+/*
+思路:首先，我们将列表中的区间按照左端点升序排序。然后我们将第一个区间加入merged数组中，并按顺序依次考虑之后
+的每个区间：
+如果当前区间的左端点在数组merged中最后一个区间的右端点之后，那么它们不会重合，我们可以直接将这个区间加入数组
+merged的末尾；
+否则，它们重合，我们需要用当前区间的右端点更新数组merged中最后一个区间的右端点，将其置为二者的较大值。
+*/
+
+// Merge 时间复杂度O(NlogN), 空间复杂度O(logN)
+func Merge(intervals [][]int) [][]int {
+	n := len(intervals)
+	if n <= 1 {
+		return intervals
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	var merged [][]int
+	for i := 0; i < n; i++ {
+		size := len(merged)
+		if size == 0 || merged[size-1][1] < intervals[i][0] {
+			merged = append(merged, intervals[i])
+		} else {
+			merged[size-1][1] = Utils.Max(merged[size-1][1], intervals[i][1])
+		}
+	}
+	return merged
+}
