399d72e29482c3ff321f24232af877453e4517d3
qiujun <qiujun@travelsky.com>
2021-12-15 19:45:58 +0800

    add new examples

--- Greedy/greedy.go
+++ Greedy/greedy.go
@@ -977,3 +977,61 @@ func MonotoneIncreasingDigits(n int) int {
 	res, _ := strconv.Atoi(string(ss))
 	return res
 }
+
+/*
+1.17 买卖股票的最佳时机含手续费
+给定一个整数数组prices，其中第i个元素代表了第i天的股票价格 ；非负整数fee代表了交易股票的手续费用。
+你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续
+购买股票了。
+
+返回获得利润的最大值。
+
+注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+
+示例1:
+输入: prices = [1, 3, 2, 8, 4, 9], fee = 2 输出: 8
+解释: 能够达到的最大利润: 在此处买入 prices[0] = 1 在此处卖出 prices[3] = 8 在此处买入 prices[4] = 4
+在此处卖出 prices[5] = 9 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+注意:
+0 < prices.length <= 50000.
+0 < prices[i] < 50000.
+0 <= fee < 50000.
+*/
+
+/*
+思路:贪心
+本题不同于1.4 买卖股票的最佳时机II在于每次交易股票有手续费的成本，所以不能按照原来累加每天的正利润思路来做。
+因为这样可能会有多笔手续费的成本，而实际最低点买进最高点卖出只有一笔手续费。
+
+我们在做收获利润操作的时候其实有三种情况：
+情况一：收获利润的这一天并不是收获利润区间里的最后一天（不是真正的卖出，相当于持有股票），所以后面要继续收获
+利润。
+情况二：前一天是收获利润区间里的最后一天（相当于真正的卖出了），今天要重新记录最小价格了。
+情况三：不作操作，保持原有状态（买入，卖出，不买不卖）
+*/
+
+// MaxProfitIncludeFee 时间复杂度O(N), 空间复杂度O(1)
+func MaxProfitIncludeFee(prices []int, fee int) int {
+	minPrice := prices[0]
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		// 不能卖出,重新记录最低股票价格
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+		// 卖掉股票的收益不足以覆盖买入和手续费的成本，也不能卖出
+		if prices[i] >= minPrice && prices[i]-minPrice-fee <= 0 {
+			continue
+		}
+		// 卖掉股票的收益足以覆盖买入和手续费的成本，才能卖出
+		if profit := prices[i] - minPrice - fee; profit > 0 {
+			// 累加交易的正利润
+			maxProfit += profit
+			// 可能有多次计算利润，最后一次计算利润才是真正意义的卖出
+			// 所以为了防止重复的手续费成本,minPrice要更新为prices[i]-fee
+			minPrice = prices[i] - fee
+		}
+	}
+	return maxProfit
+}
