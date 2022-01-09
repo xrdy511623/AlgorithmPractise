c855e013e9a634cba9139116ef24c8511a4c5da3
qiujun <qiujun@travelsky.com>
2022-01-09 18:15:35 +0800

    modify a little

--- DynamicProgramming/Medium/subSequence.go
+++ DynamicProgramming/Medium/subSequence.go
@@ -466,7 +466,7 @@ func MaxSubArray(nums []int) int {
 func MaxSubArraySimple(nums []int) int {
 	max := nums[0]
 	for i := 1; i < len(nums); i++ {
-		if nums[i]+nums[i-1] > nums[i] {
+		if nums[i-1] > 0 {
 			nums[i] += nums[i-1]
 		}
 		if max < nums[i] {
