8df3cc35e87c5f2566ea45811c8253a925fac241
qiujun <qiujun@travelsky.com>
2022-01-24 11:31:52 +0800

    modify a little

--- DynamicProgramming/Hard/hard.go
+++ DynamicProgramming/Hard/hard.go
@@ -35,35 +35,73 @@ func TrapBrutal(height []int) int {
 暴力解法时间复杂度较高是因为需要对每个下标位置都向两边扫描。如果已经知道每个位置两边的最大高度，则可以在O(n)
 的时间内得到能接的雨水总量。使用动态规划的方法，可以在O(n)的时间内预处理得到每个位置两边的最大高度。
 
-创建两个长度为n的数组leftMax和rightMax。对于0≤i<n，leftMax[i] 表示下标i及其左边的位置中，height的最大高度，
-rightMax[i]表示下标i及其右边的位置中，height的最大高度。
+创建两个长度为n的数组leftMax和rightMax。对于0≤i<n，leftMax[i] 表示下标i左边的位置中，height的最大高度，
+rightMax[i]表示下标i右边的位置中，height的最大高度。
 
-显然，leftMax[0]=height[0]，rightMax[n−1]=height[n−1]。两个数组的其余元素的计算如下：
+显然，leftMax[1]=height[0]，rightMax[n−2]=height[n−1]。两个数组的其余元素的计算如下：
 
-当1≤i≤n−1时，leftMax[i]=max(leftMax[i−1],height[i])；
+当2≤i≤n−2时，leftMax[i]=max(leftMax[i−1],height[i-1])；
 
-当0≤i≤n−2时，rightMax[i]=max(rightMax[i+1],height[i])。
+当1≤i≤n−3时，rightMax[i]=max(rightMax[i+1],height[i+1])。
 
 因此可以正向遍历数组height得到数组leftMax的每个元素值，反向遍历数组height得到数组rightMax的每个元素值。
-在得到数组leftMax和rightMax的每个元素值之后，对于0≤i<n，下标i处能接的雨水量等于
-min(leftMax[i],rightMax[i])−height[i]。遍历每个下标位置即可得到能接的雨水总量。
+在得到数组leftMax和rightMax的每个元素值之后，对于1≤i<=n-2，下标i处能接的雨水量等于
+min(leftMax[i], rightMax[i])−height[i]。遍历每个下标位置即可得到能接的雨水总量。
 */
 
 func TrapUseDp(height []int) int {
 	sum := 0
 	n := len(height)
+	// 至少需要3根柱子才能积水
+	if n <= 2 {
+		return 0
+	}
 	leftMax, rightMax := make([]int, n), make([]int, n)
-	leftMax[0], rightMax[n-1] = height[0], height[n-1]
-	for i := 1; i < n; i++ {
-		leftMax[i] = Utils.Max(leftMax[i-1], height[i])
+	leftMax[1], rightMax[n-2] = height[0], height[n-1]
+	for i := 2; i < n-1; i++ {
+		leftMax[i] = Utils.Max(height[i-1], leftMax[i-1])
 	}
-	for i := n - 2; i >= 0; i-- {
-		rightMax[i] = Utils.Max(rightMax[i+1], height[i])
+	for j := n - 3; j >= 1; j-- {
+		rightMax[j] = Utils.Max(height[j+1], rightMax[j+1])
 	}
-	for i := 1; i < n; i++ {
-		if value := Utils.Min(leftMax[i], rightMax[i]) - height[i]; value > 0 {
+	for k := 1; k <= n-2; k++ {
+		if value := Utils.Min(leftMax[k], rightMax[k]) - height[k]; value > 0 {
 			sum += value
 		}
+	}
+	return sum
+}
+
+/*
+思路:单调递减栈
+维护一个单调递减栈，如果当前柱子高度h大于栈顶元素对应的高度(s[n-1])，由于是单调递减栈，此时在栈顶元素对应
+的柱子便形成了低洼处，即s[n-1]<s[n-2]且s[n-1]<h，积水量便等于积水区域的宽度*高度。
+width = i - left - 1  i为当前柱子高度h所对应的下标, left为s[n-2]对应的下标。
+height = min(s[n-2], h)
+*/
+
+func TrapUseStack(height []int) int {
+	var stack []int
+	sum := 0
+	for i, h := range height {
+		for len(stack) > 0 && h > height[stack[len(stack)-1]] {
+			// 显然会在stack[len(stack)-1]处形成积水，积水处柱子高度为height[stack[len(stack)-1]]
+			low := height[stack[len(stack)-1]]
+			stack = stack[:len(stack)-1]
+			// 只有右侧高，是无法形成积水的
+			if len(stack) == 0 {
+				break
+			}
+			// 积水处左侧位置为stack[len(stack)-1]
+			left := stack[len(stack)-1]
+			// 积水区域宽度为积水处左侧柱子位置与右侧柱子位置之差-1
+			curWidth := i - left - 1
+			// 积水区域高度便等于积水处左侧高度与右侧高度的最小值-低洼处高度
+			curHeight := Utils.Min(height[left], h) - low
+			// 此处积水区域的面积即为此处收集到的雨水量
+			sum += curWidth * curHeight
+		}
+		stack = append(stack, i)
 	}
 	return sum
 }
@@ -75,7 +113,7 @@ func TrapUseDp(height []int) int {
 rightMax是从右往左计算，因此可以使用双指针和两个变量代替两个数组。
 
 维护两个指针left和right，以及两个变量leftMax和rightMax，初始时left=0,right=n−1,leftMax=0,rightMax=0。
-指针left只会向右移动，指针right只会向左移动，在移动指针的过程中维护两个变量 leftMax和rightMax的值。
+指针left只会向右移动，指针right只会向左移动，在移动指针的过程中维护两个变量leftMax和rightMax的值。
 
 当两个指针没有相遇时，进行如下操作：
 使用height[left]和height[right]的值更新leftMax和rightMax的值；
