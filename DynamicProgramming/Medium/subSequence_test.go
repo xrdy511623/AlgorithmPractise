6f3ff5bbd38513aa506e1f6b1643182e4406911c
qiujun <qiujun@travelsky.com>
2022-01-25 15:05:45 +0800

    add new examples

--- DynamicProgramming/Medium/subSequence_test.go
+++ DynamicProgramming/Medium/subSequence_test.go
@@ -9,9 +9,20 @@ func TestMaxSubarraySumCircular(t *testing.T) {
 	nums := []int{5, -3, 5}
 	got := MaxSubarraySumCircular(nums)
 	want := 10
-	if got != want{
+	if got != want {
 		t.Errorf("got:%v,expected:%v", got, want)
-	}else{
+	} else {
+		fmt.Println("test pass")
+	}
+}
+
+func TestNumberOfArithmeticSlices(t *testing.T) {
+	nums := []int{1, 3, 5, 7, 9, 10, 13, 15, 17, 19, 20, 21, 22, 23, 24}
+	got := NumberOfArithmeticSlices(nums)
+	want := 19
+	if got != want {
+		t.Errorf("got:%v,expected:%v", got, want)
+	} else {
 		fmt.Println("test pass")
 	}
 }
