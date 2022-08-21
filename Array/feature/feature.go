235501d1adf2c11f7fc18cb6c8311c6a161ae4d9
qiujun <jun.qiu@ushow.media>
2022-08-21 12:18:17 +0800

    enhance code

--- Array/feature/feature.go
+++ Array/feature/feature.go
@@ -21,13 +21,11 @@ func ReverseArraySimple(nums []int) []int {
 }
 
 // ReverseArray 原地反转
-func ReverseArray(nums []int) {
-	n := len(nums)
-	for i := 0; i < n/2; i++ {
-		temp := nums[n-1-i]
-		nums[n-1-i] = nums[i]
-		nums[i] = temp
+func ReverseArray(nums []int) []int {
+	for i, n := 0, len(nums); i < n/2; i++ {
+		nums[i], nums[n-1-i] = nums[n-1-i], nums[i]
 	}
+	return nums
 }
 
 /*
--- Array/sort/sort.go
+++ Array/sort/sort.go
@@ -1,7 +1,6 @@
 package sort
 
 import (
-	"AlgorithmPractise/Array/feature"
 	"AlgorithmPractise/Utils"
 	"sort"
 )
@@ -1106,7 +1105,6 @@ func MyPowSimple(x float64, n int) float64 {
   [18, 21, 23, 26, 30]
 ]
 给定target=5，返回true。
-
 给定target=20，返回false。
 */
 
@@ -1141,7 +1139,7 @@ func FindNumberIn2DArrayBinary(matrix [][]int, target int) bool {
 如果num和target 相等，返回true
 如果num大于target，列下标减 1
 如果num小于target，行下标加 1
-循环体执行完毕仍未找到元素等于 target ，说明不存在这样的元素，返回 false
+循环执行完毕仍未找到元素等于target ，说明不存在这样的元素，返回false
 */
 
 // FindNumberIn2DArray 时间复杂度为O(N+M)，空间复杂度O(1)
@@ -1194,7 +1192,7 @@ nums已按非递减顺序 排序
 
 func SortedSquares(nums []int) []int {
 	n := len(nums)
-	res := make([]int, len(nums))
+	res := make([]int, n)
 	l, r, index := 0, n-1, n-1
 	for l <= r && index >= 0 {
 		if nums[l]*nums[l] > nums[r]*nums[r] {
@@ -1236,22 +1234,21 @@ func SortedSquares(nums []int) []int {
 /*
 我们希望下一个数比当前数大，这样才满足“下一个排列”的定义。因此只需要将后面的「大数」与前面的「小数」交换，就能得到
 一个更大的数。比如123456，将5和6交换就能得到一个更大的数123465。
-我们还希望下一个数增加的幅度尽可能的小，这样才满足“下一个排列与当前排列紧邻“的要求。为了满足这个要求，我们需要：
+我们还希望下一个数增加的幅度尽可能地小，这样才满足“下一个排列与当前排列紧邻“的要求。为了满足这个要求，我们需要：
 在尽可能靠右的低位进行交换，需要从后向前查找。
-将一个尽可能小的「大数」与前面的「小数」交换。比如123465，下一个排列应该把5和4交换而不是把6和4交换
-将「大数」换到前面后，需要将「大数」后面的所有数重置为升序，升序排列就是最小的排列。以 123465 为例：
-首先按照上一步，交换5和4，得到 123564；然后需要将5之后的数重置为升序，得到123546。显然123546比123564更小，
+将一个尽可能小的大数与前面的小数交换。比如123465，下一个排列应该把5和4交换而不是把6和4交换。
+将大数换到前面后，需要将大数后面的所有数重置为升序，升序排列就是最小的排列。以123465为例：
+首先按照上一步，交换5和4，得到123564；然后需要将5之后的数重置为升序，得到123546。显然123546比123564更小，
 123546就是123465的下一个排列。
 以上就是求“下一个排列”的分析过程。
 
 算法过程
 标准的“下一个排列”算法可以描述为：
-1 从后向前查找第一个相邻升序的元素对 (i,j)，满足 A[i] < A[j]。此时 [j,end) 必然是降序
-2 在 [j,end) 从后向前查找第一个满足 A[i] < A[k]的 k。A[i]、A[k] 分别就是上文所说的「小数」、「大数」
-3 将 A[i] 与 A[k] 交换
+1 从后向前查找第一个相邻升序的元素对(i,j)，满足 A[i] < A[j]。此时 [j,end) 必然是降序
+2 在[j,end) 从后向前查找第一个满足 A[i] < A[k]的 k。A[i]、A[k] 分别就是上文所说的小数、大数
+3 将A[i]与A[k]交换
 4 可以断定这时 [j,end) 必然是降序，逆置 [j,end)，使其升序
 如果在步骤1找不到符合的升序相邻元素对，说明当前 [begin,end) 为一个降序顺序，则直接跳到步骤4
-
 */
 
 func NextPermutation(nums []int) {
@@ -1267,7 +1264,7 @@ func NextPermutation(nums []int) {
 		}
 		nums[i], nums[j] = nums[j], nums[i]
 	}
-	feature.ReverseArray(nums[i+1:])
+	Utils.ReverseArray(nums[i+1:])
 }
 
 /*
