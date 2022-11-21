d6c01080920d981d42d1cb08d2a63457c0ec98af
qiujun <jun.qiu@ushow.media>
2022-11-21 20:54:12 +0800

    add new examples

--- Array/feature/feature.go
+++ Array/feature/feature.go
@@ -640,11 +640,8 @@ func ReverseStr(s string, k int) string {
 }
 
 func reverse(s []byte) {
-	n := len(s)
-	for i := 0; i < n/2; i++ {
-		temp := s[n-1-i]
-		s[n-1-i] = s[i]
-		s[i] = temp
+	for i, n := 0, len(s); i < n/2; i++ {
+		s[i], s[n-1-i] = s[n-1-i], s[i]
 	}
 }
 
--- Array/feature/string.go
+++ Array/feature/string.go
@@ -1,6 +1,9 @@
 package feature
 
-import "AlgorithmPractise/Utils"
+import (
+	"AlgorithmPractise/Utils"
+	"strconv"
+)
 
 /*
 字符串变位词(异位词)专题
@@ -337,4 +340,49 @@ func LengthOfLongestSubstring(s string) int {
 		maxLength = Utils.Max(maxLength, i-start+1)
 	}
 	return maxLength
+}
+
+/*
+leetcode 43. 字符串相乘
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数。
+
+示例 1:
+输入: num1 = "2", num2 = "3"
+输出: "6"
+
+示例2:
+输入: num1 = "123", num2 = "456"
+输出: "56088"
+
+
+提示：
+1 <= num1.length, num2.length <= 200
+num1 和 num2 只能由数字组成。
+num1 和 num2 都不包含任何前导零，除了数字0本身。
+*/
+
+func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+	m, n := len(num1), len(num2)
+	arr := make([]int, m+n)
+	for i := m - 1; i >= 0; i-- {
+		n1 := int(num1[i] - '0')
+		for j := n - 1; j >= 0; j-- {
+			n2 := int(num2[j] - '0')
+			sum := n1*n2 + arr[i+j+1]
+			arr[i+j+1] = sum % 10
+			arr[i+j] += sum / 10
+		}
+	}
+	res := ""
+	for i, v := range arr {
+		if i == 0 && v == 0 {
+			continue
+		}
+		res += strconv.Itoa(v)
+	}
+	return res
 }
