14280d5f2e604b710888a4358887a9b777e0a9e5
qiujun <jun.qiu@ushow.media>
2022-11-22 19:24:48 +0800

    add new examples

--- Array/sort/sort.go
+++ Array/sort/sort.go
@@ -1399,6 +1399,46 @@ func mySqrt(x int) int {
 }
 
 /*
+leetcode 74 搜索二维矩阵
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+
+示例1:
+输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+输出：true
+
+示例2:
+输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+输出：false
+*/
+
+/*
+思路: 二分查找
+若将矩阵每一行拼接在上一行的末尾，则会得到一个升序数组，我们可以在该数组上二分找到目标元素。
+时间复杂度O(logm*n)
+空间复杂度O(1)
+*/
+
+func searchMatrix(matrix [][]int, target int) bool {
+	rows, columns := len(matrix), len(matrix[0])
+	l, r := 0, rows*columns-1
+	for l <= r {
+		mid := (l + r) / 2
+		curRow := mid / columns
+		curCol := mid - curRow*columns
+		if matrix[curRow][curCol] == target {
+			return true
+		} else if matrix[curRow][curCol] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return false
+}
+
+/*
 leetcode 240 搜索二维矩阵 II
 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
 每行的元素从左到右升序排列。
@@ -1425,7 +1465,7 @@ leetcode 240 搜索二维矩阵 II
 空间复杂度：O(1)。
 */
 
-func searchMatrix(matrix [][]int, target int) bool {
+func searchMatrixSec(matrix [][]int, target int) bool {
 	m, n := len(matrix), len(matrix[0])
 	x, y := 0, n-1
 	for x < m && y >= 0 {
