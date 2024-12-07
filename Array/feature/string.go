1f5c41855fbad4ded3272c99b8a371a2784b3826
qiujun <jun.qiu@ushow.media>
2024-12-07 15:01:02 +0800

    feat:add new examples

--- Array/feature/string.go
+++ Array/feature/string.go
@@ -542,3 +542,100 @@ func repeat(s string, count int) string {
 	}
 	return res.String()
 }
+
+/*
+leetcode 468 验证IP地址
+给定一个字符串 queryIP。如果是有效的 IPv4 地址，返回 "IPv4" ；如果是有效的 IPv6 地址，返回 "IPv6" ；
+如果不是上述类型的 IP 地址，返回 "Neither" 。
+
+有效的IPv4地址 是 “x1.x2.x3.x4” 形式的IP地址。 其中 0 <= xi <= 255 且 xi 不能包含 前导零。
+例如: “192.168.1.1” 、 “192.168.1.0” 为有效IPv4地址， “192.168.01.1” 为无效IPv4地址; “192.168.1.00” 、
+“192.168@1.1” 为无效IPv4地址。
+
+一个有效的IPv6地址 是一个格式为“x1:x2:x3:x4:x5:x6:x7:x8” 的IP地址，其中:
+1 <= xi.length <= 4
+xi 是一个 十六进制字符串，可以包含数字、小写英文字母( 'a' 到 'f' )和大写英文字母( 'A' 到 'F' )。
+在 xi 中允许前导零。
+例如 "2001:0db8:85a3:0000:0000:8a2e:0370:7334" 和 "2001:db8:85a3:0:0:8A2E:0370:7334" 是有效的 IPv6 地址，
+而 "2001:0db8:85a3::8A2E:037j:7334" 和 "02001:0db8:85a3:0000:0000:8a2e:0370:7334" 是无效的 IPv6 地址。
+
+示例 1：
+输入：queryIP = "172.16.254.1"
+输出："IPv4"
+解释：有效的 IPv4 地址，返回 "IPv4"
+
+示例 2：
+输入：queryIP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+输出："IPv6"
+解释：有效的 IPv6 地址，返回 "IPv6"
+
+示例 3：
+输入：queryIP = "256.256.256.256"
+输出："Neither"
+解释：既不是 IPv4 地址，又不是 IPv6 地址
+
+提示：
+queryIP 仅由英文字母，数字，字符 '.' 和 ':' 组成。
+*/
+
+func validIPAddress(queryIP string) string {
+	if checkIpV4(queryIP) {
+		return "IPv4"
+	}
+	if checkIpV6(queryIP) {
+		return "IPv6"
+	}
+	return "Neither"
+}
+
+func checkIpV4(queryIP string) bool {
+	ipParts := strings.Split(queryIP, ".")
+	// 必须是“x1.x2.x3.x4” 形式的IP地址,也就是被.分隔为4部分
+	if len(ipParts) != 4 {
+		return false
+	}
+	for _, part := range ipParts {
+		// 每部分长度在1到3之间
+		if len(part) < 1 || len(part) > 3 {
+			return false
+		}
+		// 每部分不能含前导零
+		if len(part) > 1 && part[0] == '0' {
+			return false
+		}
+		// 每部分都是数字，且值在0~255之间
+		num := 0
+		for _, ch := range part {
+			// 判断是否为合法的数字
+			if !(ch >= '0' && ch <= '9') {
+				return false
+			}
+			num = num*10 + int(ch-'0')
+		}
+		if num < 0 || num > 255 {
+			return false
+		}
+	}
+	return true
+}
+
+func checkIpV6(queryIP string) bool {
+	ipParts := strings.Split(queryIP, ":")
+	// 一个有效的IPv6地址 是一个格式为“x1:x2:x3:x4:x5:x6:x7:x8” 的IP地址,也就是被:分隔为8部分
+	if len(ipParts) != 8 {
+		return false
+	}
+	for _, part := range ipParts {
+		// 每部分长度在1~4之间
+		if len(part) < 1 || len(part) > 4 {
+			return false
+		}
+		// 每个部分包含数字、小写英文字母( 'a' 到 'f' )和大写英文字母( 'A' 到 'F' )
+		for _, ch := range part {
+			if !(ch >= '0' && ch <= '9') && !(ch >= 'a' && ch <= 'f') && !(ch >= 'A' && ch <= 'F') {
+				return false
+			}
+		}
+	}
+	return true
+}
