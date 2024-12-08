d5039a98528b54944e88a9e7a66a20bc624e209b
qiujun <jun.qiu@ushow.media>
2024-12-08 11:14:48 +0800

    feat:add new examples

--- LinkedList/Feature/feature.go
+++ LinkedList/Feature/feature.go
@@ -340,6 +340,28 @@ func insertionSortList(head *Entity.ListNode) *Entity.ListNode {
 random 指针指向链表中的任意节点或者 null。
 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+leetcode 138 随机链表的复制
+给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next
+指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针
+都不应指向原链表中的节点 。
+
+例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+返回复制链表的头节点。
+
+用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+val：一个表示 Node.val 的整数。
+random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+你的代码 只 接受原链表的头节点 head 作为传入参数。
+
+输入：head = [[3,null],[3,0],[3,null]]
+输出：[[3,null],[3,0],[3,null]]
+
+提示：
+0 <= n <= 1000
+-104 <= Node.val <= 104
+Node.random 为 null 或指向链表中的节点。
 */
 
 type Node struct {
@@ -365,6 +387,70 @@ func copyRandomList(head *Node) *Node {
 		return newNode
 	}
 	return deepCopy(head)
+}
+
+/*
+思路：迭代 + 节点拆分
+复制链表节点：我们首先遍历原链表，在每个节点后插入一个新的节点，这个新节点是该原节点的复制。这样，我们在同一个链表中形成了
+一个“交替”结构：
+原节点 -> 新节点 -> 原节点 -> 新节点 -> …
+
+例如，对于原链表 head -> A -> B -> C，复制后的链表结构将是：
+head -> A' -> A -> B' -> B -> C' -> C
+
+复制 random 指针：由于新节点是紧跟在原节点后的，我们可以通过原节点的 random 指针来直接访问新节点。具体做法是：
+
+对于每个节点的原 random 指针，如果原节点的 random 指向节点 X，那么新节点的 random 就应该指向 X 的下一个节点。
+也就是说，对于原节点 node 和新节点 node'，如果 node.random 指向 otherNode，那么 node'.random 应该指向 otherNode.next。
+
+分离两个链表：在完成复制后，原链表和新链表是交替插入的。接下来，我们需要将它们分开，使得新链表成为一个独立的链表。
+
+具体步骤：
+在原链表节点后插入复制节点。
+设置复制节点的 random 指针。
+将原链表和复制链表分开。
+时间复杂度：
+每次操作都只遍历链表一次，因此时间复杂度是 O(n)。
+空间复杂度：
+我们只使用了常数空间来处理指针和临时变量，因此空间复杂度是 O(1)。
+*/
+
+func copyRandomListSimple(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	// 第一步：在每个节点后面插入一个新的节点
+	cur := head
+	for cur != nil {
+		newNode := &Node{
+			Val:  cur.Val,
+			Next: cur.Next,
+		}
+		cur.Next = newNode
+		cur = newNode.Next
+	}
+	// 第二步：复制 random 指针
+	cur = head
+	for cur != nil {
+		if cur.Random != nil {
+			cur.Next.Random = cur.Random.Next
+		}
+		cur = cur.Next.Next
+	}
+	// 第三步：分离原链表和新链表
+	newHead := head.Next
+	curOld := head
+	curNew := newHead
+	for curNew != nil {
+		curOld.Next = curOld.Next.Next // 还原原链表
+		if curNew.Next != nil {
+			curNew.Next = curNew.Next.Next // 设置新链表的 next
+		}
+		curOld = curOld.Next
+		curNew = curNew.Next
+	}
+
+	return newHead
 }
 
 /*
