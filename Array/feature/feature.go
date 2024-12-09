7dd992d530f551ed4899388b833126ca62ff67e6
qiujun <jun.qiu@ushow.media>
2024-12-09 19:49:01 +0800

    feat:add new examples

--- Array/feature/feature.go
+++ Array/feature/feature.go
@@ -2010,3 +2010,59 @@ func mergeComplex(l, r, mid int, arr, temp []int) int {
 	}
 	return count
 }
+
+/*
+leetcode 75 颜色分类
+给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地 对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色
+顺序排列。
+
+我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+必须在不使用库内置的 sort 函数的情况下解决这个问题。
+
+示例 1：
+输入：nums = [2,0,2,1,1,0]
+输出：[0,0,1,1,2,2]
+
+示例 2：
+输入：nums = [2,0,1]
+输出：[0,1,2]
+
+提示：
+n == nums.length
+1 <= n <= 300
+nums[i] 为 0、1 或 2
+
+进阶：
+你能想出一个仅使用常数空间的一趟扫描算法吗？
+*/
+
+/*
+遍历一遍数组，统计红白蓝三种颜色元素出现的次数
+然后按照它们的出现次数，从下标0开始赋值即可。
+*/
+
+func sortColors(nums []int) {
+	red, white, blue := 0, 0, 0
+	for i, n := 0, len(nums); i < n; i++ {
+		if nums[i] == 0 {
+			red++
+		} else if nums[i] == 1 {
+			white++
+		} else {
+			blue++
+		}
+	}
+	index := 0
+	for i := 0; i < red; i++ {
+		nums[index] = 0
+		index++
+	}
+	for i := 0; i < white; i++ {
+		nums[index] = 1
+		index++
+	}
+	for i := 0; i < blue; i++ {
+		nums[index] = 2
+		index++
+	}
+}
