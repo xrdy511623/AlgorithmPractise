5843c88d1f554fafb9ee915dc783e8cb3af0d4fd
qiujun <jun.qiu@ushow.media>
2024-12-10 20:17:49 +0800

    feat:add new examples

--- Array/sort/sort.go
+++ Array/sort/sort.go
@@ -2,6 +2,7 @@ package sort
 
 import (
 	"AlgorithmPractise/Utils"
+	"container/heap"
 	"math"
 	"sort"
 	"strconv"
@@ -1985,4 +1986,122 @@ func findKthNumber(n int, k int) int {
 		}
 	}
 	return cur
+}
+
+/*
+leetcode 295 数据流的中位数
+中位数是有序整数列表中的中间值。如果列表的大小是偶数，则没有中间值，中位数是两个中间值的平均值。
+
+例如 arr = [2,3,4] 的中位数是 3 。
+例如 arr = [2,3] 的中位数是 (2 + 3) / 2 = 2.5 。
+实现 MedianFinder 类:
+
+MedianFinder() 初始化 MedianFinder 对象。
+
+void addNum(int num) 将数据流中的整数 num 添加到数据结构中。
+
+double findMedian() 返回到目前为止所有元素的中位数。与实际答案相差 10-5 以内的答案将被接受。
+
+示例 1：
+
+输入
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+输出
+[null, null, null, 1.5, null, 2.0]
+
+解释
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // 返回 1.5 ((1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+提示:
+-105 <= num <= 105
+在调用 findMedian 之前，数据结构中至少有一个元素
+最多 5 * 104 次调用 addNum 和 findMedian
+*/
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int {
+	return len(h)
+}
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h MaxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	n := len(*h)
+	x := (*h)[n-1]
+	*h = (*h)[:n-1]
+	return x
+}
+
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	n := len(*h)
+	x := (*h)[n-1]
+	*h = (*h)[:n-1]
+	return x
+}
+
+type MedianFinder struct {
+	maxHeap *MaxHeap
+	minHeap *MinHeap
+}
+
+func Constructor() MedianFinder {
+	return MedianFinder{
+		maxHeap: &MaxHeap{},
+		minHeap: &MinHeap{},
+	}
+}
+
+func (mf *MedianFinder) AddNum(num int) {
+	if mf.maxHeap.Len() == 0 || num <= (*mf.maxHeap)[0] {
+		heap.Push(mf.maxHeap, num)
+	} else {
+		heap.Push(mf.minHeap, num)
+	}
+	if mf.maxHeap.Len() > mf.minHeap.Len()+1 {
+		heap.Push(mf.minHeap, heap.Pop(mf.maxHeap))
+	} else if mf.minHeap.Len() > mf.maxHeap.Len() {
+		heap.Push(mf.maxHeap, heap.Pop(mf.minHeap))
+	}
+}
+
+func (mf *MedianFinder) FindMedian() float64 {
+	if mf.maxHeap.Len() > mf.minHeap.Len() {
+		return float64((*mf.maxHeap)[0])
+	}
+	return float64((*mf.maxHeap)[0]+(*mf.minHeap)[0]) / 2.0
 }
