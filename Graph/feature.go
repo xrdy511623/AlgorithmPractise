b01a80b0ba8db215566acd4aaf7dba1805ebc3c3
qiujun <jun.qiu@ushow.media>
2024-12-12 11:29:45 +0800

    feat:add new examples

--- Graph/feature.go
+++ Graph/feature.go
@@ -128,3 +128,74 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	// 如果处理的课程数量等于总课程数，说明没有环，所有课程都可以完成
 	return preProcessCount == numCourses
 }
+
+/*
+leetcode 210 课程表II
+现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，其中
+prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前必须先选修 bi 。
+
+例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，
+返回 一个空数组 。
+
+示例 1：
+输入：numCourses = 2, prerequisites = [[1,0]]
+输出：[0,1]
+解释：总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+
+示例 2：
+输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+输出：[0,2,1,3]
+解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+
+示例 3：
+输入：numCourses = 1, prerequisites = []
+输出：[0]
+
+提示：
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= numCourses * (numCourses - 1)
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+ai != bi
+所有[ai, bi] 互不相同
+*/
+
+/*
+思路:拓扑排序
+本题与leetcode 207 课程表 几乎如出一辙，处理当前入度为0的课程其实就是课程安排的学习顺序
+所以代码很容易就写出来了。
+*/
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	graph := make([][]int, numCourses)
+	inDegree := make([]int, numCourses)
+	for _, preReq := range prerequisites {
+		pre, course := preReq[1], preReq[0]
+		graph[pre] = append(graph[pre], course)
+		inDegree[course]++
+	}
+	queue := []int{}
+	for i := 0; i < numCourses; i++ {
+		if inDegree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+	order := []int{}
+	for len(queue) > 0 {
+		course := queue[0]
+		queue = queue[1:]
+		order = append(order, course)
+		for _, neighbor := range graph[course] {
+			inDegree[neighbor]--
+			if inDegree[neighbor] == 0 {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+	if len(order) == numCourses {
+		return order
+	}
+	return []int{}
+}
